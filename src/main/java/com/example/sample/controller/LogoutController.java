64cd3211847f21bc9d7439cf0509ed86e47dd385
yuta-iiduka <ota12041204@gmail.com>
2021-12-06 23:11:18 +0900

    add layout

---
+++ src/main/java/com/example/sample/controller/LogoutController.java
@@ -0,0 +1,18 @@
+package com.example.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+
+
+@Controller
+@Slf4j
+public class LogoutController {
+	@PostMapping("/logout")
+	public String postLogout() {
+		return "redirect:/login";
+	}
+}
